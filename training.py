f47485b9632ee853b2f04e4e98cf94c6a0fe79f8
Sardor2118 <mo3rus@gmail.com>
2024-01-30 12:19:18 +0500

    Второй коммит

--- training.py
+++ training.py
@@ -1,3 +1,20 @@
-t = (1, 2)
-b = 2 * t
-print(b)
+import sqlite3
+connection1 = sqlite3.connect('users.db')
+sql = connection1.cursor()
+sql.execute('CREATE TABLE IF NOT EXISTS users (user_id INTEGER, username TEXT);')
+sql.execute('INSERT INTO users (user_id, username) VALUES (1992, "Sardor");')
+
+
+
+id_users = sql.execute('SELECT user_id FROM users;').fetchall()
+print(id_users)
+where = sql.execute('SELECT * FROM users WHERE username="Sardor";').fetchall()
+print(where)
+# delete = sql.execute('DELETE FROM users;')
+# print(delete)
+
+
+eba = sql.execute('UPDATE users SET username="SWW" WHERE user_id=1992;')
+print(eba)
+
+connection1.commit()
--- урок 13/urok_13.py
+++ урок 13/urok_13.py
@@ -11,6 +11,20 @@ connection = sqlite3.connect('my_users.db')
 # подключаемся к базе данных
 sql = connection.cursor()
 # создаем использинеля для работы с sql
-sql.execute("")
+sql.execute('CREATE TABLE cars (name TEXT, model TEXT, year INTEGER);')
 # даём команды нашей базе данных
+connection.commit()
+# сохранение данных после изменений
+connection.close()
+# закрываем соеденение с базой данных
+
+# SELECT - выбирает колонку чтобы получить данных из таблицы
+# FROM - выбирает таблицу из которой выбрать данные
+# WHERE - фильтрация искать в колоннах
+# UPDATE - обновление колон в таблице
+# SET - новое значение чтобы обновить
+
+# .execute('CREATE TABLE cars (name TEXT, model TEXT, year INTEGER);')
+# sql.execute('CREATE TABLE IF NOT EXISTS users (user_id INTEGER, username TEXT);')
+# sql.execute('UPDATE cars SET year=2011 WHERE model='tesla';)
 
